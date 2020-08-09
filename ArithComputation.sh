d46d6cc63d927c1af14a63c7c8e5a02cd453d42a
pradip0016 <patilpradip980@gmail.com>
2020-08-09 12:36:41 +0530

    uc8

--- ArithComputation.sh
+++ ArithComputation.sh
@@ -12,15 +12,35 @@ echo "A: "$a
 echo "B: "$b
 echo "C: "$c
 declare -A dict
-dict[UC2]=$(( $a + $b * $c ))
-dict[UC3]=$(( $a * $b + $c ))
-dict[UC4]=$(( $c + $a / $b ))
-dict[UC5]=$(( $a % $b + $c ))
+dict[0]=$(( $a + $b * $c ))
+dict[1]=$(( $a * $b + $c ))
+dict[2]=$(( $c + $a / $b ))
+dict[3]=$(( $a % $b + $c ))
 
-count=0
-for value in ${dict[@]}
+#store
+
+for (( i=0 ; i<${#dict[@]} ; i++ ))
 do
-    array[$count]=$value
-    ((count++))
+nos[$i]=${dict[$i]}
 done
-echo "Array values: " ${array[@]}
+
+#print array
+for ((i=0 ; i<${#dict[@]} ; i++))
+do
+echo ${nos[$i]}
+done
+
+#Descending order 
+for (( i=0 ; i<${#dict[@]} ; i++ ))
+do
+for (( j=i+1 ; j<${#dict[@]} ; j++ ))
+do
+if [ ${nos[i]} -lt ${nos[j]} ]
+then
+temp=${nos[i]}
+nos[i]=${nos[j]}
+nos[j]=$temp
+fi
+done
+done
+echo "number after sort in Descending : ${nos[*]}"
