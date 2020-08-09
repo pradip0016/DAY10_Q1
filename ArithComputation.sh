10780a7f80e198c24ea092538c59f41874bd5a4b
pradip0016 <patilpradip980@gmail.com>
2020-08-09 11:17:38 +0530

    input

--- ArithComputation.sh
+++ ArithComputation.sh
@@ -2,3 +2,13 @@
 #This problem computes different Arithmetic expressions and sorts the result
 
 echo " This problem computes different Arithmetic expressions and sorts the results "
+
+
+echo "enter three input value: "
+read a
+read b
+read c
+echo "A: "$a
+echo "B: "$b
+echo "C: "$c
+
